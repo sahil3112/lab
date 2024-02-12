82040c3d88c452c01da056b63a1202be3a0406a2
sahil gupta <43255158+sahil3112@users.noreply.github.com>
2024-02-13 00:46:23 +0530

    Create ThreatMapper_server_setup.sh

---
+++ ThreatMapper_server_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt-get update
+apt-get install unzip
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+wget https://github.com/deepfence/ThreatMapper/archive/refs/tags/threatintel-yara-2024-02-12_01-26-18.zip
+unzip threatintel-yara-2024-02-12_01-26-18.zip
+cd /home/ubuntu/ThreatMapper-threatintel-yara-2024-02-12_01-26-18/deployment-scripts
+docker compose up -d
+sleep 180
